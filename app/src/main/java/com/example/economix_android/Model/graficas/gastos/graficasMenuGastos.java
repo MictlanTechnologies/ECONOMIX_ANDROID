7f8a81f779b02799a82122965d2916e810630ed4
MictlanTechnologies <mictlantechnologies@gmail.com>
2025-10-15 11:35:37 -0600

    Ya esata trminado el esqueleto, ya cualquier cosa se irá agregando sobre la marcha

--- app/src/main/java/com/example/economix_android/Model/graficas/gastos/graficasMenuGastos.java
+++ app/src/main/java/com/example/economix_android/Model/graficas/gastos/graficasMenuGastos.java
@@ -1,18 +1,22 @@
-package com.example.economix_android.Model.usuario;
+
+package com.example.economix_android.Model.graficas.gastos;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.economix_android.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link usuario#newInstance} factory method to
+ * Use the {@link graficasMenuGastos#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class usuario extends Fragment {
+public class graficasMenuGastos extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,26 +27,26 @@ public class usuario extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    public graficasMenuGastos() {
+        // Required empty public constructor
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment usuario.
+     * @return A new instance of fragment graficasMenuGastos.
      */
     // TODO: Rename and change types and number of parameters
-    public static usuario newInstance(String param1, String param2) {
-        usuario fragment = new usuario();
+    public static graficasMenuGastos newInstance(String param1, String param2) {
+        graficasMenuGastos fragment = new graficasMenuGastos();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public usuario() {
-        // Required empty public constructor
     }
 
     @Override
@@ -58,6 +62,6 @@ public class usuario extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario, container, false);
+        return inflater.inflate(R.layout.fragment_graficas_menu_gastos, container, false);
     }
 }
