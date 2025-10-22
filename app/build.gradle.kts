8e0d6aec504af4c6605bd0def7ea8775a0cdfed2
MictlanTechnologies <solanorojasgabriel@gmail.com>
2025-10-21 21:36:35 -0600

    hubo mejoras significativas en el diseño del frontend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.android.material:material:1.13.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
