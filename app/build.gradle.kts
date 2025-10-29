39acde9fdded065d1a141c44b5bbd0de68098dbf
Solan26007 <solanorojasgabriel@gmail.com>
2025-10-29 14:49:12 -0600

    Arreglo de ImView en menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.google.android.material:material:1.13.0")
+    implementation("com.google.android.material:material:1.5.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
