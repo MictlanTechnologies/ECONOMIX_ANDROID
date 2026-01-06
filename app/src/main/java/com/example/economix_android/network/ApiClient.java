cfebe886c379c79886973106eef70b590b511d65
MictlanTechnologies <mictlantechnologies@gmail.com>
2026-01-06 17:55:00 -0600

    Algunos arreglos

--- app/src/main/java/com/example/economix_android/network/ApiClient.java
+++ app/src/main/java/com/example/economix_android/network/ApiClient.java
@@ -30,7 +30,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public final class ApiClient {
 
     // IMPORTANTE: la URL base debe terminar con "/" para Retrofit
-    private static final String BASE_URL = "http://192.168.1.159:8080/";
+    private static final String BASE_URL = "http://192.168.1.159" +
+            ":8080/";
     private static final Retrofit retrofit;
 
     static {
